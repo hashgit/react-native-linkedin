9bcb20fd44b7c00c8a56e5173ca8a18a8f12c161
Xavier Carpentier <xcapetir@gmail.com>
2017-07-11 00:13:39 +0200

    feat(example): add example

---
+++ example/App.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import {
+  Platform,
+  StatusBar,
+  StyleSheet,
+  View,
+  Image,
+  Dimensions,
+  TouchableOpacity,
+} from 'react-native'
+
+import LinkedInModal from 'react-native-linkedin'
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  statusBarUnderlay: {
+    height: 24,
+    backgroundColor: 'rgba(0,0,0,0.2)',
+  },
+  img: {
+    width: Dimensions.get('window').width - 30,
+    resizeMode: 'contain',
+  },
+})
+
+export default class AppContainer extends React.Component {
+  state = {
+    linkedInModalOpen: false,
+  }
+  render() {
+    return (
+      <View style={styles.container}>
+        <TouchableOpacity
+          onPress={() => this.setState({ linkedInModalOpen: true })}
+        >
+          <Image source={require('./assets/linkedin.png')} style={styles.img} />
+        </TouchableOpacity>
+
+        <LinkedInModal visible={this.state.linkedInModalOpen} />
+
+        {Platform.OS === 'ios' && <StatusBar barStyle="default" />}
+        {Platform.OS === 'android' && <View style={styles.statusBarUnderlay} />}
+      </View>
+    )
+  }
+}
